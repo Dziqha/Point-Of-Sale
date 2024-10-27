a188b52fe02cf923fb10f6306e1770a5537f9b79
gbagush <galihbagushardiyanto@gmail.com>
2024-10-27 09:00:32 +0700

    change transaction logic

--- core/controllers/transaction_item.py
+++ core/controllers/transaction_item.py
@@ -12,8 +12,26 @@ def create_transaction_item(transaction_id: int, product_id: int, quantity: int,
         discount=Decimal(discount),
         total=Decimal(total)
     )
-    result = transaction_item.create()
-    return result
+    response = transaction_item.create()
+    
+    if response["status"] == "success" and "data" in response:
+        return {
+            "status": response["status"],
+            "message": response["message"],
+            "data": {
+                "transaction_item_id": response["data"]["transaction_item_id"],
+                "transaction_id": response["data"]["transaction_id"],
+                "product_id": response["data"]["product_id"],
+                "quantity": response["data"]["quantity"],
+                "price": response["data"]["price"],
+                "discount": response["data"]["discount"],
+                "total": response["data"]["total"]
+            }
+        }
+    return {
+        "status": response["status"],
+        "message": response["message"]
+    }
 
 @eel.expose
 def update_transaction_item(transaction_item_id: int, product_id: int, quantity: int, price: float, discount: float, total: float):
@@ -53,18 +71,23 @@ def get_transaction_item_by_id(transaction_item_id: int):
         discount=Decimal(0), 
         total=Decimal(0)
     )
-    transaction_item_data = transaction_item.get_by_id(transaction_item_id)
-    if transaction_item_data:
+    response = transaction_item.get_by_id(transaction_item_id)
+
+    if response["status"] == "success" and "data" in response:
         return {
-            "transaction_item_id": transaction_item_data[0],
-            "transaction_id": transaction_item_data[1],
-            "product_id": transaction_item_data[2],
-            "quantity": transaction_item_data[3],
-            "price": str(transaction_item_data[4]),
-            "discount": str(transaction_item_data[5]),
-            "total": str(transaction_item_data[6])
+            "status": response["status"],
+            "message": response["message"],
+            "data": {
+                "transaction_item_id": response["data"]["transaction_item_id"],
+                "transaction_id": response["data"]["transaction_id"],
+                "product_id": response["data"]["product_id"],
+                "quantity": response["data"]["quantity"],
+                "price": response["data"]["price"],
+                "discount": response["data"]["discount"],
+                "total": response["data"]["total"]
+            }
         }
-    return {"status": "failed", "message": "Transaction item not found."}
+    return {"status": "error", "message": "Transaction item not found."}
 
 @eel.expose
 def get_all_transaction_items_by_transaction_id(transaction_id: int):
@@ -76,17 +99,24 @@ def get_all_transaction_items_by_transaction_id(transaction_id: int):
         discount=Decimal(0), 
         total=Decimal(0)
     )
-    transaction_items_data = transaction_item.get_all_by_transaction_id(transaction_id)
-    return [
-        {
-            "transaction_item_id": ti[0],
-            "transaction_id": ti[1],
-            "product_id": ti[2],
-            "product_name": ti[3],
-            "product_sku": ti[4],
-            "quantity": ti[5],
-            "price": str(ti[6]),
-            "discount": str(ti[7]),
-            "total": str(ti[8])
-        } for ti in transaction_items_data
-    ]
+    response = transaction_item.get_all_by_transaction_id(transaction_id)
+
+    if response["status"] == "success" and "data" in response:
+        return {
+            "status": response["status"],
+            "message": response["message"],
+            "data": [
+                {
+                    "transaction_item_id": ti["transaction_item_id"],
+                    "transaction_id": ti["transaction_id"],
+                    "product_id": ti["product_id"],
+                    "product_name": ti["product_name"],
+                    "product_sku": ti["product_sku"],
+                    "quantity": ti["quantity"],
+                    "price": ti["price"],
+                    "discount": ti["discount"],
+                    "total": ti["total"]
+                } for ti in response["data"]
+            ]
+        }
+    return {"status": "error", "message": "No transaction items found for the given transaction ID."}
--- core/services/transaction_item.py
+++ core/services/transaction_item.py
@@ -74,7 +74,19 @@ class TransactionItem(BaseModel):
             conn.commit()
 
             if cursor.rowcount > 0:
-                return {"status": "success", "message": "Transaction item saved to database and stock updated."}
+                return {
+                    "status": "success",
+                    "message": "Transaction item saved to database and stock updated.",
+                    "data": {
+                        "transaction_item_id": cursor.lastrowid,
+                        "transaction_id": self.transaction_id,
+                        "product_id": self.product_id,
+                        "quantity": self.quantity,
+                        "price": str(price),
+                        "discount": str(discount_percentage),
+                        "total": str(total)
+                    }
+                }
             else:
                 return {"status": "error", "message": "Failed to save transaction item or product not found."}
 
@@ -85,76 +97,114 @@ class TransactionItem(BaseModel):
         finally:
             conn.close()
 
-    def delete(self) -> str:
+    def delete(self) -> dict:
         if self.transaction_item_id is None:
-            return "Transaction item ID is not set."
+            return {"status": "error", "message": "Transaction item ID is not set."}
 
         conn, cursor = db.init_db()
         if conn is None or cursor is None:
-            return "Database connection error."
+            return {"status": "error", "message": "Database connection error."}
 
         cursor.execute('''DELETE FROM transaction_items WHERE transaction_item_id = ?''', (self.transaction_item_id,))
         conn.commit()
-        result = f"Transaction item with ID {self.transaction_item_id} deleted from database." if cursor.rowcount > 0 else "Failed to delete transaction item."
+        result = {
+            "status": "success" if cursor.rowcount > 0 else "error",
+            "message": f"Transaction item with ID {self.transaction_item_id} deleted from database." if cursor.rowcount > 0 else "Failed to delete transaction item."
+        }
         conn.close()
 
         return result
 
-    def get_by_id(self, id: int) -> Optional[Tuple[Any]]:
+    def get_by_id(self, id: int) -> dict:
         conn, cursor = db.init_db()
         if conn is None or cursor is None:
-            print("Database connection error.")
-            return None
+            return {"status": "error", "message": "Database connection error."}
 
         cursor.execute('''SELECT * FROM transaction_items WHERE transaction_item_id = ?''', (id,))
         transaction_item = cursor.fetchone()
         conn.close()
-        
-        return transaction_item
 
-    def get_all_by_transaction_id(self, transaction_id: int) -> List[Tuple[Any]]:
-            conn, cursor = db.init_db()
-            if conn is None or cursor is None:
-                print("Database connection error.")
-                return []
+        if transaction_item:
+            return {
+                "status": "success",
+                "message": "Transaction item found.",
+                "data": {
+                    "transaction_item_id": transaction_item[0],
+                    "transaction_id": transaction_item[1],
+                    "product_id": transaction_item[2],
+                    "price": str(transaction_item[3]),
+                    "discount": str(transaction_item[4]),
+                    "quantity": transaction_item[5],
+                    "total": str(transaction_item[6])
+                }
+            }
+        else:
+            return {"status": "error", "message": "Transaction item not found."}
 
-            cursor.execute('''
-                SELECT 
-                    ti.transaction_item_id,
-                    ti.transaction_id,
-                    ti.product_id,
-                    p.name AS product_name,
-                    p.sku AS product_sku,
-                    ti.quantity,
-                    ti.price,
-                    ti.discount,
-                    ti.total
-                FROM 
-                    transaction_items ti
-                JOIN 
-                    products p ON ti.product_id = p.product_id
-                WHERE 
-                    ti.transaction_id = ?
-            ''', (transaction_id,))
-            transaction_items = cursor.fetchall()
-            conn.close()
+    def get_all_by_transaction_id(self, transaction_id: int) -> dict:
+        conn, cursor = db.init_db()
+        if conn is None or cursor is None:
+            return {"status": "error", "message": "Database connection error."}
 
-            return transaction_items
+        cursor.execute('''
+            SELECT 
+                ti.transaction_item_id,
+                ti.transaction_id,
+                ti.product_id,
+                p.name AS product_name,
+                p.sku AS product_sku,
+                ti.quantity,
+                ti.price,
+                ti.discount,
+                ti.total
+            FROM 
+                transaction_items ti
+            JOIN 
+                products p ON ti.product_id = p.product_id
+            WHERE 
+                ti.transaction_id = ?
+        ''', (transaction_id,))
+        transaction_items = cursor.fetchall()
+        conn.close()
 
-    def update(self) -> str:
+        if transaction_items:
+            return {
+                "status": "success",
+                "message": "Transaction items found.",
+                "data": [
+                    {
+                        "transaction_item_id": row[0],
+                        "transaction_id": row[1],
+                        "product_id": row[2],
+                        "product_name": row[3],
+                        "product_sku": row[4],
+                        "quantity": row[5],
+                        "price": str(row[6]),
+                        "discount": str(row[7]),
+                        "total": str(row[8])
+                    } for row in transaction_items
+                ]
+            }
+        else:
+            return {"status": "error", "message": "No transaction items found for the given transaction ID."}
+
+    def update(self) -> dict:
         if self.transaction_item_id is None:
-            return "Transaction item ID is not set."
+            return {"status": "error", "message": "Transaction item ID is not set."}
 
         conn, cursor = db.init_db()
         if conn is None or cursor is None:
-            return "Database connection error."
+            return {"status": "error", "message": "Database connection error."}
 
         cursor.execute('''UPDATE transaction_items 
                           SET product_id = ?, quantity = ?, price = ?, discount = ?, total = ? 
                           WHERE transaction_item_id = ?''', 
                        (self.product_id, self.quantity, self.price, self.discount, self.total, self.transaction_item_id))
         conn.commit()
-        result = f"Transaction item with ID {self.transaction_item_id} updated in database." if cursor.rowcount > 0 else "Failed to update transaction item."
+        result = {
+            "status": "success" if cursor.rowcount > 0 else "error",
+            "message": f"Transaction item with ID {self.transaction_item_id} updated in database." if cursor.rowcount > 0 else "Failed to update transaction item."
+        }
         conn.close()
 
         return result
